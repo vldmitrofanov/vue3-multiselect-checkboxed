dc11e73ec0f86fd1fce17616fd3946fb9f3006aa
Vladimir Mitrofanov <shirker2006@gmail.com>
2023-03-01 03:16:12 +0800

    first commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Vue3MultiselectCheckboxed from "./components/Vue3MultiselectCheckboxed.vue"
-
-export default {
-    install: (app, options) => {
-        app.component("Vue3MultiselectCheckboxed", Vue3MultiselectCheckboxed)
-    }
-}
-
-export { Vue3MultiselectCheckboxed }
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import Plugin from "./index"
-
-const app = createApp(App)
-app.use(Plugin)
-app.mount('#app')
